983afcc1a18f634b33341994cb64b477c60fe1af
Ben Haney <ben@benhaney.com>
2017-11-03 11:25:12 -0500

    Remove slashes from room name
    Closes #1

--- public/js/front-page.js
+++ public/js/front-page.js
@@ -2,5 +2,6 @@ $('form#join-container').addEventListener('submit', e => {
   e.preventDefault()
   let room = $('form#join-container input[name="room"]').value
   if (!room) room = randomPhrase(3)
+  room = room.replace(/\//g, '')
   window.location.replace('//'+window.location.host+'/'+room)
 })
