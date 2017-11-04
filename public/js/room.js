f72b762ba5c6dfef1b21301e355172368cf0a79d
Ben Haney <ben@benhaney.com>
2017-11-03 20:31:33 -0500

    Update vid-self moving

--- public/js/room.js
+++ public/js/room.js
@@ -126,32 +126,10 @@ $('.mute-button').addEventListener('click', () => {
 })
 
 $('#vid_self').addEventListener('click', () => {
-  if($('#vid_self').classList.contains('bottom-right')) {
-    //bottom right -> bottom left
-    $('#vid_self').style.right = ""
-    $('#vid_self').style.left = "10px"
-    $('#vid_self').classList.remove("bottom-right");
-    $('#vid_self').classList.add("bottom-left");
-  }
-  else if($('#vid_self').classList.contains('bottom-left')) {
-    //bottom left -> top left
-    $('#vid_self').style.bottom = ""
-    $('#vid_self').style.top = "10px"
-    $('#vid_self').classList.remove("bottom-left");
-    $('#vid_self').classList.add("top-left");
-  }
-  else if($('#vid_self').classList.contains('top-left')) {
-    //top left -> top right
-    $('#vid_self').style.left = ""
-    $('#vid_self').style.right = "10px"
-    $('#vid_self').classList.remove("top-left");
-    $('#vid_self').classList.add("top-right");
-  }
-  else if($('#vid_self').classList.contains('top-right')) {
-    //top right -> bottom right
-    $('#vid_self').style.top = ""
-    $('#vid_self').style.bottom = "10px"
-    $('#vid_self').classList.remove("top-right");
-    $('#vid_self').classList.add("bottom-right");
-  }
+  $('#vid_self').setAttribute('data-pos', ({
+    'bottom-right': 'top-right',
+    'top-right'   : 'top-left',
+    'top-left'    : 'bottom-left',
+    'bottom-left' : 'bottom-right'
+  })[$('#vid_self').getAttribute('data-pos')])
 })
